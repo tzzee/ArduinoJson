ee09c35f037de8367cdbffe8c14dba5cb16f26ff
Kenji Takahashi <takahashi@noex-inc.co.jp>
2024-04-17 13:27:13 +0900

    Force use of SPIRAM on ESP32

--- src/ArduinoJson/Document/DynamicJsonDocument.hpp
+++ src/ArduinoJson/Document/DynamicJsonDocument.hpp
@@ -7,21 +7,36 @@
 #include <ArduinoJson/Document/BasicJsonDocument.hpp>
 
 #include <stdlib.h>  // malloc, free
+#ifdef ARDUINO_ARCH_ESP32
+#include <esp_heap_caps.h>
+#endif
 
 namespace ARDUINOJSON_NAMESPACE {
 
 // The allocator of DynamicJsonDocument.
 struct DefaultAllocator {
   void* allocate(size_t size) {
+#ifdef ARDUINO_ARCH_ESP32
+    return heap_caps_malloc(size, MALLOC_CAP_DEFAULT);
+#else
     return malloc(size);
+#endif
   }
 
   void deallocate(void* ptr) {
+#ifdef ARDUINO_ARCH_ESP32
+    heap_caps_free(ptr);
+#else
     free(ptr);
+#endif
   }
 
   void* reallocate(void* ptr, size_t new_size) {
+#ifdef ARDUINO_ARCH_ESP32
+    return heap_caps_realloc(ptr, new_size, MALLOC_CAP_DEFAULT);
+#else
     return realloc(ptr, new_size);
+#endif
   }
 };
 
--- src/ArduinoJson/Document/StaticJsonDocument.hpp
+++ src/ArduinoJson/Document/StaticJsonDocument.hpp
@@ -5,9 +5,65 @@
 #pragma once
 
 #include <ArduinoJson/Document/JsonDocument.hpp>
+#ifdef ARDUINO_ARCH_ESP32
+#include <esp_heap_caps.h>
+#endif
 
 namespace ARDUINOJSON_NAMESPACE {
 
+#ifdef ARDUINO_ARCH_ESP32
+template <size_t desiredCapacity>
+class StaticJsonDocument : public JsonDocument {
+  static const size_t _capacity =
+      AddPadding<Max<1, desiredCapacity>::value>::value;
+
+ public:
+  StaticJsonDocument() : JsonDocument(_buffer = (char*)heap_caps_malloc(_capacity, MALLOC_CAP_DEFAULT), _capacity) {}
+
+  StaticJsonDocument(const StaticJsonDocument& src)
+      : JsonDocument(_buffer = heap_caps_malloc(_capacity, MALLOC_CAP_DEFAULT), _capacity) {
+    set(src);
+  }
+
+  template <typename T>
+  StaticJsonDocument(
+      const T& src,
+      typename enable_if<is_convertible<T, JsonVariantConst>::value>::type* = 0)
+      : JsonDocument(_buffer = (char*)heap_caps_malloc(_capacity, MALLOC_CAP_DEFAULT), _capacity) {
+    set(src);
+  }
+
+  // disambiguate
+  StaticJsonDocument(JsonVariant src) : JsonDocument(_buffer = (char*)heap_caps_malloc(_capacity, MALLOC_CAP_DEFAULT), _capacity) {
+    set(src);
+  }
+
+  ~StaticJsonDocument() {
+    heap_caps_free((void*)_buffer);
+  }
+
+  StaticJsonDocument& operator=(const StaticJsonDocument& src) {
+    set(src);
+    return *this;
+  }
+
+  template <typename T>
+  StaticJsonDocument& operator=(const T& src) {
+    set(src);
+    return *this;
+  }
+
+  // Reclaims the memory leaked when removing and replacing values.
+  // https://arduinojson.org/v6/api/jsondocument/garbagecollect/
+  void garbageCollect() {
+    StaticJsonDocument tmp(*this);
+    set(tmp);
+  }
+
+ private:
+  char* _buffer;
+};
+#else
 // A JsonDocument with a memory pool on the stack.
 template <size_t desiredCapacity>
 class StaticJsonDocument : public JsonDocument {
@@ -56,5 +112,6 @@ class StaticJsonDocument : public JsonDocument {
  private:
   char _buffer[_capacity];
 };
+#endif
 
 }  // namespace ARDUINOJSON_NAMESPACE
